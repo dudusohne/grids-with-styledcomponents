957e9550fa8b75b2bdf42609b03cecc7e26e87ca
Eduardo Sohne <dudu.hit@gmail.com>
2021-10-25 14:43:28 -0300

    add a header with grid

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,10 +8,13 @@ export function Header() {
         <ContainerHeader>
             <Grid style={{ margin: '0 auto', padding: '0', width: '100%', height: '100%' }}>
                 <Row style={{ padding: '1rem 1rem 1rem 1rem' }}>
-                    <Col xs={2} style={{ backgroundColor: 'red', height: '100%' }}>1</Col>
-                    <Col xs={4} style={{ backgroundColor: 'blue' }}>1</Col>
-                    <Col xs={4} style={{ backgroundColor: 'green' }}>1</Col>
+                    <Col xs={2} style={{ backgroundColor: 'red', height: '7vh' }}>1</Col>
+                    <Col xs={2} style={{ backgroundColor: 'blue' }}>1</Col>
+                    <Col xs={3} style={{ backgroundColor: 'green' }}>1</Col>
                     <Col xs={2} style={{ backgroundColor: 'yellow' }}>1</Col>
+                    <Col xs={1} style={{ backgroundColor: 'gray' }}>1</Col>
+                    <Col xs={1} style={{ backgroundColor: 'purple' }}>1</Col>
+                    <Col xs={1} style={{ backgroundColor: 'brown' }}>1</Col>
                 </Row>
             </Grid>
         </ContainerHeader>
