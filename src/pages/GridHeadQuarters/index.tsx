4f2ece1de7a75f97bf0cb9fc5426309e589f263a
Eduardo Sohne <dudu.hit@gmail.com>
2021-10-25 14:58:39 -0300

    Update index.tsx

--- src/pages/GridHeadQuarters/index.tsx
+++ src/pages/GridHeadQuarters/index.tsx
@@ -6,7 +6,7 @@ import { Circle, Rotate } from './grid.styles';
 export function GridHeadQuarters() {
 
     return (
-        <Grid style={{ margin: '0 auto', padding: '0', width: '100%', height: '100%' }}>
+        <Grid style={{ margin: '0 auto', padding: '0', width: '100%', height: '40vh' }}>
             <Header />
             <Row end="xs" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <Col xs={6} style={{ backgroundColor: 'purple', display: 'flex', justifyContent: 'center' }}>
