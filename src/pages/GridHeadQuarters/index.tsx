20dd1fc2c5eaf06296efa5d47fc5b907e7f10789
Eduardo Sohne <dudu.hit@gmail.com>
2021-10-25 14:45:46 -0300

    Update index.tsx

--- src/pages/GridHeadQuarters/index.tsx
+++ src/pages/GridHeadQuarters/index.tsx
@@ -8,15 +8,11 @@ export function GridHeadQuarters() {
     return (
         <Grid style={{ margin: '0 auto', padding: '0', width: '100%', height: '100%' }}>
             <Header />
-            <Row>
-                <Col xs={12} style={{ backgroundColor: 'pink', height: '10rem' }}>
-                    <Row end="xs" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        <Col xs={6} style={{ backgroundColor: 'purple', display: 'flex', justifyContent: 'center' }}>
-                            <Circle />
-                            <Rotate><Circle /></Rotate>
-                            <Circle />
-                        </Col>
-                    </Row>
+            <Row end="xs" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Col xs={6} style={{ backgroundColor: 'purple', display: 'flex', justifyContent: 'center' }}>
+                    <Circle />
+                    <Rotate><Circle /></Rotate>
+                    <Circle />
                 </Col>
             </Row>
         </Grid>
